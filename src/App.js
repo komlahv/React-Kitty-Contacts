c2f4311772e7ea12c6d7b7ddd384b90d417c038f
VictorEKomlah <komlahv@gmail.com>
2019-08-05 16:42:01 +0000

    added contional rendering and fetch

--- src/App.js
+++ src/App.js
@@ -2,30 +2,61 @@ import React from 'react';
 import ContactCard from "./ContactCard";
 import Header from "./Components/Header";
 
-function App() {
-    return (
-        <div>
-            <Header />
-            <div className="contacts">
-                <ContactCard
-                    contact={{ name: "Mr. Whiskerson", imgUrl: "http://placekitten.com/300/200", phone: "(212) 555-1234", email: "mr.whiskaz@catnap.meow" }}
-                />
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            unreadMessages: ["a", "b"],
+            character: {},
+        };
+    }
 
-                <ContactCard
-                    contact={{ name: "Fluffykins", imgUrl: "http://placekitten.com/400/200", phone: "(212) 555-2345", email: "fluff@me.com" }}
-                />
+    componentDidMount() {
+        fetch("https://swapi.co/api/people/1")
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    character: data
+                })
+            })
+            .catch(e => console.log(e))
+    }
 
-                <ContactCard
-                    contact={{ name: "Destroyer", imgUrl: "http://placekitten.com/400/300", phone: "(212) 555-3456", email: "ofworlds@yahoo.com" }}
-                />
+    render() {
+        return (
+            <div>
+                <Header />
+                <div className="contacts">
+                    <ContactCard
+                        contact={{ name: "Mr. Whiskerson", imgUrl: "http://placekitten.com/300/200", phone: "(212) 555-1234", email: "mr.whiskaz@catnap.meow" }}
+                    />
 
-                <ContactCard
-                    contact={{ name: "Felix", imgUrl: "http://placekitten.com/200/100", phone: "(212) 555-4567", email: "thecat@hotmail.com" }}
-                />
+                    <ContactCard
+                        contact={{ name: "Fluffykins", imgUrl: "http://placekitten.com/400/200", phone: "(212) 555-2345", email: "fluff@me.com" }}
+                    />
 
+                    <ContactCard
+                        contact={{ name: "Destroyer", imgUrl: "http://placekitten.com/400/300", phone: "(212) 555-3456", email: "ofworlds@yahoo.com" }}
+                    />
+
+                    <ContactCard
+                        contact={{ name: "Felix", imgUrl: "http://placekitten.com/200/100", phone: "(212) 555-4567", email: "thecat@hotmail.com" }}
+                    />
+
+                </div>
+                <div>
+                    {
+                        //conditional rendering - if there are unread messages show text else don't
+                        this.state.unreadMessages.length > 0 &&
+                        <h2>You have {this.state.unreadMessages.length} unread messages!</h2>
+                    }
+                </div>
+                <div>
+                    <p>{this.state.character.name}</p>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default App;
